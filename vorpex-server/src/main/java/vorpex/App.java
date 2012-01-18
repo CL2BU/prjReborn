464133485eeb0ce968fa1397c4925a63be81f576
d.gunn@vorpex.biz <d.gunn@vorpex.biz>
2012-01-18 16:49:25 +0000

    Initial commit of project base.

---
+++ vorpex-server/src/main/java/vorpex/App.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * <copyright file="App.java" company="VorpeX">
+ * Copyright (c) 2011-2012 All Right Reserved, http://vorpex.biz/
+ * 
+ * This source is subject to the "Don't Be A Dick" License.
+ * Please see the License.txt file for more information.
+ * You may not use this file except in compliance with the License.
+ * 
+ * THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY 
+ * KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+ * PARTICULAR PURPOSE.
+ * 
+ * @author Dominic Gunn
+ * @email d.gunn@vorpex.biz
+ * @date 18-12-2012
+ * @summary
+ ******************************************************************************/
+
+package vorpex;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ vorpex-server/src/test/java/vorpex/AppTest.java
@@ -0,0 +1,57 @@
+/*******************************************************************************
+ * <copyright file="AppTest.java" company="VorpeX">
+ * Copyright (c) 2011-2012 All Right Reserved, http://vorpex.biz/
+ * 
+ * This source is subject to the "Don't Be A Dick" License.
+ * Please see the License.txt file for more information.
+ * You may not use this file except in compliance with the License.
+ * 
+ * THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY 
+ * KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+ * PARTICULAR PURPOSE.
+ * 
+ * @author Dominic Gunn
+ * @email d.gunn@vorpex.biz
+ * @date 18-12-2012
+ * @summary
+ ******************************************************************************/
+
+package vorpex;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
